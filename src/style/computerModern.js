e0db314dfe0abc20d826d372984df1ce8764a1bb
James Smith <james.smith@djalbat.com>
2024-04-10 17:52:11 +0100

    Corrected the font style.

--- src/style/computerModern.js
+++ src/style/computerModern.js
@@ -3,140 +3,140 @@
 const computerModernStyle = `
 
   @font-face {
-    src: url("${host}font/cmunrm.woff2");
+    src: url("font/cmunrm.woff2");
     font-family: "Computer Modern Serif";
     font-weight: normal;
     font-style: normal;
   }
     
   @font-face {
-    src: url("${host}font/cmunti.woff2");
+    src: url("font/cmunti.woff2");
     font-family: "Computer Modern Serif";
     font-weight: normal;
     font-style: italic;
   }
     
   @font-face {
-    src: url("${host}font/cmunbx.woff2");
+    src: url("font/cmunbx.woff2");
     font-family: "Computer Modern Serif";
     font-weight: bold;
     font-style: normal;
   }
     
   @font-face {
-    src: url("${host}font/cmunbi.woff2");
+    src: url("font/cmunbi.woff2");
     font-family: "Computer Modern Serif";
     font-weight: bold;
     font-style: italic;
   }  
     
   @font-face {
-    src: url("${host}font/cmunss.woff2");
+    src: url("font/cmunss.woff2");
     font-family: "Computer Modern Sans";
     font-weight: normal;
     font-style: normal;
   }
   
   @font-face {
-    src: url("${host}font/cmunsi.woff2");
+    src: url("font/cmunsi.woff2");
     font-family: "Computer Modern Sans";
     font-weight: normal;
     font-style: italic;
   }
   
   @font-face {
-    src: url("${host}font/cmunsx.woff2");
+    src: url("font/cmunsx.woff2");
     font-family: "Computer Modern Sans";
     font-weight: bold;
     font-style: normal;
   }
   
   @font-face {
-    src: url("${host}font/cmunso.woff2");
+    src: url("font/cmunso.woff2");
     font-family: "Computer Modern Sans";
     font-weight: bold;
     font-style: italic;
   }
   
   @font-face {
-    src: url("${host}font/cmuntt.woff2");
+    src: url("font/cmuntt.woff2");
     font-family: "Computer Modern Typewriter";
     font-weight: normal;
     font-style: normal;
   }
   
   @font-face {
-    src: url("${host}font/cmunit.woff2");
+    src: url("font/cmunit.woff2");
     font-family: "Computer Modern Typewriter";
     font-weight: normal;
     font-style: italic;
   }
     
   @font-face {
-    src: url("${host}font/cmuntb.woff2");
+    src: url("font/cmuntb.woff2");
     font-family: "Computer Modern Typewriter";
     font-weight: bold;
     font-style: normal;
   }
     
   @font-face {
-    src: url("${host}font/cmuntx.woff2");
+    src: url("font/cmuntx.woff2");
     font-family: "Computer Modern Typewriter";
     font-weight: bold;
     font-style: italic;
   }
       
   @font-face {
-    src: url("${host}font/cmunbmr.woff2");
+    src: url("font/cmunbmr.woff2");
     font-family: "Computer Modern Bright";
     font-weight: normal;
     font-style: normal;
   }
   
   @font-face {
-    src: url("${host}font/cmunbmo.woff2");
+    src: url("font/cmunbmo.woff2");
     font-family: "Computer Modern Bright";
     font-weight: normal;
     font-style: italic;
   }
   
   @font-face {
-    src: url("${host}font/cmunbbx.woff2");
+    src: url("font/cmunbbx.woff2");
     font-family: "Computer Modern Bright";
     font-weight: bold;
     font-style: normal;
   }
   
   @font-face {
-    src: url("${host}font/cmunbxo.woff2");
+    src: url("font/cmunbxo.woff2");
     font-family: "Computer Modern Bright";
     font-weight: bold;
     font-style: italic;
   }
 
   @font-face {
-    src: url("${host}font/cmunorm.woff2");
+    src: url("font/cmunorm.woff2");
     font-family: "Computer Modern Concrete";
     font-weight: normal;
     font-style: normal;
   }
     
   @font-face {
-    src: url("${host}font/cmunoti.woff2");
+    src: url("font/cmunoti.woff2");
     font-family: "Computer Modern Concrete";
     font-weight: normal;
     font-style: italic;
   }
     
   @font-face {
-    src: url("${host}font/cmunobx.woff2");
+    src: url("font/cmunobx.woff2");
     font-family: "Computer Modern Concrete";
     font-weight: bold;
     font-style: normal;
   }
     
   @font-face {
-    src: url("${host}font/cmunobi.woff2");
+    src: url("font/cmunobi.woff2");
     font-family: "Computer Modern Concrete";
     font-weight: bold;
     font-style: italic;

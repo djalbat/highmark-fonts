be2a5087ceda5a7d348e998aac21b1c61e800e20
James Smith <james.smith@djalbat.com>
2024-04-11 19:22:43 +0100

    Changed the getFontDirectoryPath() function slightly.

--- src/getFontDirectoryPath.js
+++ src/getFontDirectoryPath.js
@@ -8,7 +8,7 @@ const { getPackagePath } = packageUtilities,
 import { FONT } from "./constants";
 
 export default function getFontDirectoryPath() {
-  const packagePath = getPackagePath(),
+  const packagePath = getPackagePath(__dirname),
         fontDirectoryPath = concatenatePaths(packagePath, FONT);
 
   return fontDirectoryPath;

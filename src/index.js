88b089c0efb8fc5f5c49524072ac37840b11e8f2
James Smith <james.smith@djalbat.com>
2025-10-14 21:39:06 +0100

    Propagated.

--- src/index.js
+++ src/index.js
@@ -1,9 +1,5 @@
 "use strict";
 
-export { default as symbolicCharacterMap } from "./characterMap/symbolic";
-export { default as symbolicCharacterMaps } from "./characterMaps/symbolic";
-export { default as alphaNumericCharacterMaps } from "./characterMaps/alphaNumeric";
-
 export { default as svgUtilities } from "./utilities/svg";
 
 export { default as stixStyle } from "./style/stix";
